6d48913e98696031d5d0ea4d79df5cd980565d53
Shawn McCabe <smccabe@acromediainc.com>
2020-06-28 16:51:53 -0700

    formatting fixes

--- src/loot.mjs
+++ src/loot.mjs
@@ -3,46 +3,46 @@ import Item from './item.mjs';
 import util from './util.mjs';
 
 class Loot {
-    static roll(power) {
-        let item = Load.randomItem(power);
-        item = new Item(item.name, item.type, item.damage, item.armour);
+  static roll(power) {
+    let item = Load.randomItem(power);
+    item = new Item(item.name, item.type, item.damage, item.armour);
 
-        const magic = util.getRandomInt(10);
-        if(magic < 3) {
-            item = this.magic(item);
-        }
-
-        util.saveItem(item);
-
-        return item;
+    const magic = util.getRandomInt(10);
+    if (magic < 3) {
+      item = this.magic(item);
     }
 
-    static magic(item) {
-        const roll = util.getRandomInt(100);
-        if(roll < 50) {
-            const prefix = Load.randomItemPrefix();
-            item.enchantPrefix(prefix);
-            item.magic = "Uncommon";
-            return item;
-        }
+    util.saveItem(item);
 
-        const suffix = Load.randomItemSuffix();
-        item.enchantSuffix(suffix);
-        item.magic = "Magic";
+    return item;
+  }
 
-        if(roll > 85) {
-            const suffix = Load.randomItemPrefix();
-            item.enchantPrefix(suffix);
-            item.magic = "Rare"
-        }
-
-        if(roll > 98) {
-            item.lengendary();
-            item.magic = "Legendary";
-        }
-
-        return item;
+  static magic(item) {
+    const roll = util.getRandomInt(100);
+    if (roll < 50) {
+      const prefix = Load.randomItemPrefix();
+      item.enchantPrefix(prefix);
+      item.magic = 'Uncommon';
+      return item;
     }
+
+    let suffix = Load.randomItemSuffix();
+    item.enchantSuffix(suffix);
+    item.magic = 'Magic';
+
+    if (roll > 85) {
+      suffix = Load.randomItemPrefix();
+      item.enchantPrefix(suffix);
+      item.magic = 'Rare';
+    }
+
+    if (roll > 98) {
+      item.lengendary();
+      item.magic = 'Legendary';
+    }
+
+    return item;
+  }
 }
 
 export default Loot;
